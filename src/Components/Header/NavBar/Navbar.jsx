23ed0f57933d38926f7aa800955ef50a93b28c8c
Tilottam Zope <tilottamzope1999@gmail.com>
2024-06-03 21:06:10 +0530

    Complete Header Part

--- src/Components/Header/NavBar/Navbar.jsx
+++ src/Components/Header/NavBar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { MdMenu } from "react-icons/md";
 import { FaAngleDown } from "react-icons/fa6";
 import { Link } from "react-router-dom";
@@ -10,21 +10,37 @@ import { TbBrandSketch } from "react-icons/tb";
 import { FcAbout } from "react-icons/fc";
 import { FaRegNewspaper } from "react-icons/fa";
 const Navbar = () => {
+  const [isopenSidebarVal, setisopenSidebarVal]=useState(true);
+
   return (
     <div>
       <nav>
         <div className="container">
           <div className="row">
             <div className="col-sm-3 navPart1">
-              <Button className="allCatTab align-items-center">
-                <span className="icon1 me-2">
-                  <MdMenu />
-                </span>
-                <span className="text">ALL CATEGORIES</span>
-                <span className="icon2 ms-2">
-                  <FaAngleDown />
-                </span>
-              </Button>
+              <div className="cartWrapper">
+                <Button className="allCatTab align-items-center" onClick={()=>setisopenSidebarVal(!isopenSidebarVal)}>
+                  <span className="icon1 me-2">
+                    <MdMenu />
+                  </span>
+                  <span className="text">ALL CATEGORIES</span>
+                  <span className="icon2 ms-2">
+                    <FaAngleDown />
+                  </span>
+                </Button>
+                <div className={`sidebarNav ${isopenSidebarVal===true ? 'open' : ''}`}>
+                  <ul>
+                    <li> <Link to="/">Detol</Link></li>
+                    <li> <Link to="/">Nivea</Link></li>
+                    <li> <Link to="/">Himalaya</Link></li>
+                    <li> <Link to="/">Dabur</Link></li>
+                   <li> <Link to="/">Detol</Link></li>
+                    <li> <Link to="/">Nivea</Link></li>
+                    <li> <Link to="/">Himalaya</Link></li>
+                    <li> <Link to="/">Dabur</Link></li>
+                  </ul>
+                </div>
+              </div>
             </div>
             <div className="col-sm-9 navPart2 d-flex align-items-center">
               <ul className="list list-inline ms-auto">
@@ -53,15 +69,10 @@ const Navbar = () => {
                     <TbBrandSketch /> &nbsp;Top Brands
                   </Link>
                   <div className="submenu">
-                  <Link to="/">
-                    Detol
-                  </Link><Link to="/">
-                    Nivea
-                  </Link><Link to="/">
-                    Himalaya
-                  </Link><Link to="/">
-                    Dabur
-                  </Link>
+                    <Link to="/">Detol</Link>
+                    <Link to="/">Nivea</Link>
+                    <Link to="/">Himalaya</Link>
+                    <Link to="/">Dabur</Link>
                   </div>
                 </li>
                 <li className="list-inline-item">
