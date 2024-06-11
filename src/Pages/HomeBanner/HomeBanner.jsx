a776084bf2d8d63f6f775ac74a5ba6d5840b72f7
Tilottam Zope <tilottamzope1999@gmail.com>
2024-06-11 06:32:12 +0530

    Make A Home Banner

--- src/Pages/HomeBanner/HomeBanner.jsx
+++ src/Pages/HomeBanner/HomeBanner.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+import "./HomeBanner.css";
+import Banner1 from '../../Assets/Banner1.jpg'
+import Banner2 from '../../Assets/Banner2.jpg'
+import Banner3 from '../../Assets/Banner3.jpg'
 
-const HomeBanner = () => {
+const HomeBanner = (props) => {
+  var settings = {
+    dots : false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows : true,
+    autoplay : true
+  };
   return (
-    <div>
-      
+    <div className="container mt-3">
+      <div className="homeBannerSection">
+        <Slider {...settings}>
+          <div className="item">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/004/299/806/small/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-vector.jpg" alt="Banner Not Found" className="w-100" />
+          </div>
+          <div className="item">
+            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/online-shop-banner-design-template-ced8d381ed48595c749de9949ff917ae_screen.jpg?ts=1600552562" alt="Banner Not Found" className="w-100" />
+          </div>
+          <div className="item">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/003/240/364/small/shopping-online-on-phone-paper-art-modern-pink-background-gifts-box-free-vector.jpg" alt="Banner Not Found" className="w-100" />
+          </div>
+        </Slider>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Home.css'
+import HomeBanner from '../HomeBanner/HomeBanner'
 const Home = () => {
   return (
     <div>
-      Home Page
+      <HomeBanner/>
     </div>
   )
 }
